73b5ea064f6e2b5b627ab914a4d6259a640b2a3f
mindfulcoder49 <alex.g.alcivar49@gmail.com>
2024-03-01 09:47:53 -0500

    fix typo add elon musk mention

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
           href="https://www.llamaindex.ai/"
           className="flex items-center justify-center font-nunito text-lg font-bold gap-2"
         >
-          <span>A customGPT expert on the New York Times Lawsuit again OpenAI and Microsoft, Built for North Shore AI Developers using create-llama</span>
+          <span>A customGPT expert on the New York Times lawsuit, and Elon Musk lawsuit against OpenAI and Microsoft, Built for North Shore AI Developers using create-llama</span>
         </a>
     </div>
   );
